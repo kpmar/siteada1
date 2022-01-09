29cff343b3e93136893a5b683055238ccaa6df15
KellyMarques <>
2022-01-09 19:30:32 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var botaoEnviar = document.getElementById("botaoEnviar");
+
+botaoEnviar.addEventListener("click",validaFormulario)
+
+function validaFormulario(){
+  var nome = document.getElementById("nome").value;
+  var email = document.getElementById("email").value;
+  var telefone = document.getElementById("telefone").value;
+  if ((nome != "") &&
+      (email != "") &&
+      (telefone != "")) {
+      alert("Prontinho! Você receberá as novidades por e-mail.")
+  }else{
+      alert("Por favor, preencha os campos de nome, e-mail e telefone!")
+  }
+}
